7b52200259ebee97fb7bcc82b7f3d2fda1246494
luthfar-cv <116001120+luthfar-cv@users.noreply.github.com>
2023-02-16 15:25:26 +0530

    Update streamlit_app.py
    load fruits CSV

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit;
+import pandas
+
+
 streamlit.title('My parents New Healthy Diner')
 
 streamlit.header('BreakFast Favourites🍕🍟🍔 ')
@@ -8,6 +11,9 @@ streamlit.text('Hot dog 🌭')
 streamlit.text('Avacado toast 🥑🥪')
 
 streamlit.header('🍓🍌🍎 making my own smoothie 🍹🥤🧃')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
  
  
  
