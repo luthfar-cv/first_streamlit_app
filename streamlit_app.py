4f985d9a5e2367700c509a3565d3aa33d539b08c
luthfar-cv <116001120+luthfar-cv@users.noreply.github.com>
2023-02-16 15:42:04 +0530

    Update streamlit_app.py
    pre populate list in multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ streamlit.header('🍓🍌🍎 making my own smoothie 🍹🥤🧃')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits :", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits :", list(my_fruit_list.index), ['Avocado','Grapes'])
 streamlit.dataframe(my_fruit_list)
  
  
