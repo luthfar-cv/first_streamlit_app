c828c5218450bad78e900715877c4bc6012261ba
luthfar-cv <116001120+luthfar-cv@users.noreply.github.com>
2023-02-16 15:19:08 +0530

    Update streamlit_app.py
    add cute emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
 import streamlit;
 streamlit.title('My parents New Healthy Diner')
 
-streamlit.header('BreakFast Menu')
-streamlit.text('pizza')
-streamlit.text('Egg')
-streamlit.text('Sausages')
+streamlit.header('BreakFast Favourites🍕🍟🍔 ')
+streamlit.text('pizza 🍕')
+streamlit.text('Egg 🥚')
+streamlit.text('Hot dog 🌭')
+streamlit.text('Avacado toast 🥑🥪')
  
  
  
