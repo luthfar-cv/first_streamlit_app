d662519a985ccf6d29eed4b33fa9bd885b29aa53
luthfar-cv <116001120+luthfar-cv@users.noreply.github.com>
2023-02-16 15:54:02 +0530

    Update streamlit_app.py
    fix typo error

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ streamlit.header('🍓🍌🍎 making my own smoothie 🍹🥤🧃')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 fruit_selected = streamlit.multiselect("Pick some fruits :", list(my_fruit_list.index), ['Avocado','Grapes'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+fruits_to_show = my_fruit_list.loc[fruit_selected]
 streamlit.dataframe(fruits_to_show)
  
  
