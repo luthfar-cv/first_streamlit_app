910b7106f43ec933d263e7ba944cdc9d468b5fe1
luthfar-cv <116001120+luthfar-cv@users.noreply.github.com>
2023-02-19 02:40:13 +0530

    Update streamlit_app.py
    fix error

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 item = streamlit.text_area('Enter fruit',"");
 streamlit.write('The user entered', item);
 my_cur = my_cnx.cursor()
-cursor.execute("INSERT INTO fruit_load_list(item) VALUES (%s)", (item,))
+my_cur.execute("INSERT INTO fruit_load_list(item) VALUES (%s)", (item,))
 my_cur.commit()
 
 my_cur.execute("select * from fruit_load_list")
