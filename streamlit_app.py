3e0958a0f38792aad506c0f748a00bb04c670381
luthfar-cv <116001120+luthfar-cv@users.noreply.github.com>
2023-02-18 19:51:04 +0530

    Update streamlit_app.py
    fixed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ streamlit.header('Fruityvise Fruit Advice')
 fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
 streamlit.text(fruityvice_response.json())
 
-fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
  
  
