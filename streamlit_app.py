1ebe315f473763e6053d1023b13422d35b446647
luthfar-cv <116001120+luthfar-cv@users.noreply.github.com>
2023-02-18 19:47:56 +0530

    Update streamlit_app.py
    normalising the response

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,30 @@
 import streamlit;
 import requests
-# import pandas
+import pandas
 
 
-# streamlit.title('My parents New Healthy Diner')
+streamlit.title('My parents New Healthy Diner')
 
-# streamlit.header('BreakFast Favourites🍕🍟🍔 ')
-# streamlit.text('pizza 🍕')
-# streamlit.text('Egg 🥚')
-# streamlit.text('Hot dog 🌭')
-# streamlit.text('Avacado toast 🥑🥪')
+streamlit.header('BreakFast Favourites🍕🍟🍔 ')
+streamlit.text('pizza 🍕')
+streamlit.text('Egg 🥚')
+streamlit.text('Hot dog 🌭')
+streamlit.text('Avacado toast 🥑🥪')
 
-# streamlit.header('🍓🍌🍎 making my own smoothie 🍹🥤🧃')
+streamlit.header('🍓🍌🍎 making my own smoothie 🍹🥤🧃')
 
-# my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-# my_fruit_list = my_fruit_list.set_index('Fruit')
-# fruit_selected = streamlit.multiselect("Pick some fruits :", list(my_fruit_list.index), ['Avocado','Grapes'])
-# fruits_to_show = my_fruit_list.loc[fruit_selected]
-# streamlit.dataframe(fruits_to_show)
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+fruit_selected = streamlit.multiselect("Pick some fruits :", list(my_fruit_list.index), ['Avocado','Grapes'])
+fruits_to_show = my_fruit_list.loc[fruit_selected]
+streamlit.dataframe(fruits_to_show)
+
 streamlit.header('Fruityvise Fruit Advice')
 fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
 streamlit.text(fruityvice_response.json())
+
+fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
  
  
  
